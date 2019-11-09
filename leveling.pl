6f2fe0b491d1140cb998e0b7d2fb9bddec66777b
stanleyyoga123 <yoga360227@gmail.com>
2019-11-09 12:17:08 +0700

    Level Up

--- leveling.pl
+++ leveling.pl
@@ -1,2 +1,12 @@
-:- include('player.pl')
+:- include('player.pl').
 
+levelUp(ID) :-
+    inventory(ID,Name,Type,MaxHealth,Level,Health,Element,Attack,Special,Exp),
+    TempLevel is (Level+1),
+    TempMaxHealth is (MaxHealth*2),
+    TempHealth is TempMaxHealth,
+    TempAttack is (Attack*1.2),
+    TempSpecial is (Special*1.2),
+    TempExp is 0,
+    retract(inventory(ID,_,_,_,Level,_,_,_,_,_)),
+    asserta(inventory(ID,Name,Type,TempMaxHealth,TempLevel,TempHealth,Element,TempAttack,TempSpecial,TempExp)).
