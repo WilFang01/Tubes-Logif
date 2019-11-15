4fe476065ae72b9f419ca8c663c865c8c39e85f1
13518014Ignatius <54546012+13518014Ignatius@users.noreply.github.com>
2019-11-15 15:03:36 +0700

    capital mod (baca deskripsi)
    sekarang nama tokemon bisa (dan harus):
    1. Pake ' di depan dan belakang
    2. Pake huruf capital di depannya

--- tokedex2.pl
+++ tokedex2.pl
@@ -2,98 +2,98 @@
 /* Rencananya, bakal ada 6 Tokemon starter, 1 buat tiap Element */
 /* Player milih 1 starter, yang gak dipilih gabisa didapetin dari cara lain */
 
-tokedex(1, bulsabaur, normal, 360, 1, grass, 70, 110).
-tokedex(2, charamder, normal, 355, 1, fire, 75, 120).
-tokedex(3, squirtrel, normal, 345, 1, water, 85, 100).
-tokedex(4, cupi, normal, 370, 1, electric, 65, 115).
-tokedex(5, pigday, normal, 355, 1, wind, 70, 100).
-tokedex(6, smackhorny, normal, 400, 1, earth, 80, 95).
+tokedex(1, 'Bulsabaur', normal, 360, 1, grass, 70, 110).
+tokedex(2, 'Charamder', normal, 355, 1, fire, 75, 120).
+tokedex(3, 'Squirtrel', normal, 345, 1, water, 85, 100).
+tokedex(4, 'Cupi', normal, 370, 1, electric, 65, 115).
+tokedex(5, 'Pigday', normal, 355, 1, wind, 70, 100).
+tokedex(6, 'Smackhorny', normal, 400, 1, earth, 80, 95).
 
 /* Tokedex buat Tokemon normal yang bisa ditemuin di wild */
 /* Rencananya, starter Tokemon gabisa ditemuin di wild kayak di Pokemon */
 /* Buat Tokemon yang punya suffix -eo, gw rencananya bikin mereka gabisa evolve, jadi gw kuat in stat mereka */
 
-tokedex(7, fullpics, normal, 300, 1, fire, 40, 115).
-tokedex(8, magicrap, normal, 300, 1, water, 50, 120).
-tokedex(9, cancera, normal, 300, 1, grass, 60, 105).
-tokedex(10, helicoptile, normal, 300, 1, electric, 40, 125).
-tokedex(11, sperong, normal, 300, 1, wind, 55, 110).
-tokedex(12, tylite, normal, 300, 1, earth, 50, 85).
-tokedex(13, flareo, normal, 350, 1, fire, 55, 185).
-tokedex(14, aquoeo, normal, 350, 1, water, 75, 185).
-tokedex(15, natureo, normal, 350, 1, grass, 75, 185).
-tokedex(16, volteo, normal, 350, 1, electric, 75, 185).
-tokedex(17, aereo, normal, 350, 1, wind, 75, 185).
-tokedex(18, terreo, normal, 350, 1, earth, 75, 185).
-tokedex(19, camcam, normal, 280, 1, fire, 50, 110).
-tokedex(20, frogfrog, normal, 290, 1, water, 40, 115).
-tokedex(21, busew, normal, 280, 1, grass, 40, 120).
-tokedex(22, lightkid, normal, 270, 1, electric, 40, 135).
-tokedex(23, skyling, normal, 300, 1, wind, 55, 100).
-tokedex(24, gibel, normal, 320, 1, earth, 55, 85).
-tokedex(25, ponytak, normal, 300, 1, fire, 45, 115).
-tokedex(26, trapezoidyu, normal, 260, 1, water, 45, 135).
-tokedex(27, telorcute, normal, 320, 1, grass, 50, 95).
-tokedex(28, proton, normal, 280, 1, electric, 35, 150).
-tokedex(29, dodol, normal, 270, 1, wind, 65, 110).
-tokedex(30, oneng, normal, 270, 1, earth, 80, 90).
-tokedex(31, tes, normal, 999999, 9, earth, 9999999, 9999999).
+tokedex(7, 'Fullpics', normal, 300, 1, fire, 40, 115).
+tokedex(8, 'Magicrap', normal, 300, 1, water, 50, 120).
+tokedex(9, 'Cancera', normal, 300, 1, grass, 60, 105).
+tokedex(10, 'Helicoptile', normal, 300, 1, electric, 40, 125).
+tokedex(11, 'Sperong', normal, 300, 1, wind, 55, 110).
+tokedex(12, 'Tylite', normal, 300, 1, earth, 50, 85).
+tokedex(13, 'Flareo', normal, 350, 1, fire, 55, 185).
+tokedex(14, 'Aquoeo', normal, 350, 1, water, 75, 185).
+tokedex(15, 'Natureo', normal, 350, 1, grass, 75, 185).
+tokedex(16, 'Volteo', normal, 350, 1, electric, 75, 185).
+tokedex(17, 'Aereo', normal, 350, 1, wind, 75, 185).
+tokedex(18, 'Terreo', normal, 350, 1, earth, 75, 185).
+tokedex(19, 'Camcam', normal, 280, 1, fire, 50, 110).
+tokedex(20, 'Frogfrog', normal, 290, 1, water, 40, 115).
+tokedex(21, 'Busew', normal, 280, 1, grass, 40, 120).
+tokedex(22, 'Lightkid', normal, 270, 1, electric, 40, 135).
+tokedex(23, 'Skyling', normal, 300, 1, wind, 55, 100).
+tokedex(24, 'Gibel', normal, 320, 1, earth, 55, 85).
+tokedex(25, 'Ponytak', normal, 300, 1, fire, 45, 115).
+tokedex(26, 'Trapezoidyu', normal, 260, 1, water, 45, 135).
+tokedex(27, 'Telorcute', normal, 320, 1, grass, 50, 95).
+tokedex(28, 'Proton', normal, 280, 1, electric, 35, 150).
+tokedex(29, 'Dodol', normal, 270, 1, wind, 65, 110).
+tokedex(30, 'Oneng', normal, 270, 1, earth, 80, 90).
+tokedex(31, 'Tes', normal, 999999, 9, earth, 9999999, 9999999).
 
-tokedex(100,sangemon,legendary,100000,90,water,3000,6000).
-tokedex(101,icanmon,legendary,133243,90,fire,2843,4000).
+tokedex(100, 'Sangemon', legendary,100000,90,water,3000,6000).
+tokedex(101, 'Icanmon', legendary,133243,90,fire,2843,4000).
 
-namaSkill(bulsabaur, onyong-onyong).
-namaSkill(charamder, blar-blar).
-namaSkill(squirtrel, pyur-pyur).
-namaSkill(cupi, cup-cup).
-namaSkill(pigday, oink-oink).
-namaSkill(smackhorny, horny).
-namaSkill(ponytak, entak-entul).
-namaSkill(trapezoidyu, zung).
-namaSkill(telorcute, crot).
-namaSkill(proton, kimia-e).
-namaSkill(dodol, manis).
-namaSkill(oneng, neng).
-namaSkill(fullpics, foto-bugil).
-namaSkill(magicrap, splash).
-namaSkill(cancera, kanker).
-namaSkill(helicoptile, helikopter).
-namaSkill(sperong, terong).
-namaSkill(tylite, tytyt).
-namaSkill(flareo, geni).
-namaSkill(aquoeo, banyu).
-namaSkill(natureo, alas).
-namaSkill(volteo, gluduk).
-namaSkill(aereo, topan).
-namaSkill(terreo, earthquake).
-namaSkill(camcam, bronze-icpc).
-namaSkill(frogfrog, swikee).
-namaSkill(busew, mambu).
-namaSkill(lightkid, silau-men).
-namaSkill(skyling, cling).
-namaSkill(gibel, gembel).
+namaSkill('Bulsabaur', onyong-onyong).
+namaSkill('Charamder', blar-blar).
+namaSkill('Squirtrel', pyur-pyur).
+namaSkill('Cupi', cup-cup).
+namaSkill('Pigday', oink-oink).
+namaSkill('Smackhorny', horny).
+namaSkill('Ponytak', entak-entul).
+namaSkill('Trapezoidyu', zung).
+namaSkill('Telorcute', crot).
+namaSkill('Proton', kimia-e).
+namaSkill('Dodol', manis).
+namaSkill('Oneng', neng).
+namaSkill('Fullpics', foto-bugil).
+namaSkill('Magicrap', splash).
+namaSkill('Cancera', kanker).
+namaSkill('Helicoptile', helikopter).
+namaSkill('Sperong', terong).
+namaSkill('Tylite', tytyt).
+namaSkill('Flareo', geni).
+namaSkill('Aquoeo', banyu).
+namaSkill('Natureo', alas).
+namaSkill('Volteo', gluduk).
+namaSkill('Aereo', topan).
+namaSkill('Terreo', earthquake).
+namaSkill('Camcam', bronze-icpc).
+namaSkill('Frogfrog', swikee).
+namaSkill('Busew', mambu).
+namaSkill('Lightkid', silau-men).
+namaSkill('Skyling', cling).
+namaSkill('Gibel', gembel).
 
-maxExp(fullpics, 100).
-maxExp(magicrap, 120).
-maxExp(cancera, 105).
-maxExp(helicoptile, 125).
-maxExp(sperong, 110).
-maxExp(tylite, 95).
-maxExp(flareo, 120).
-maxExp(aquoeo, 130).
-maxExp(natureo, 125).
-maxExp(volteo, 100).
-maxExp(aereo,145).
-maxExp(terreo, 150).
-maxExp(camcam, 110).
-maxExp(frogfrog, 115).
-maxExp(busew, 120).
-maxExp(lightkid, 135).
-maxExp(skyling, 100).
-maxExp(gibel, 110).
-maxExp(ponytak, 115).
-maxExp(trapezoidyu, 135).
-maxExp(telorcute, 105).
-maxExp(proton, 150).
-maxExp(dodol, 110).
-maxExp(oneng, 120).
+maxExp('Fullpics', 100).
+maxExp('Magicrap', 120).
+maxExp('Cancera', 105).
+maxExp('Helicoptile', 125).
+maxExp('Sperong', 110).
+maxExp('Tylite', 95).
+maxExp('Flareo', 120).
+maxExp('Aquoeo', 130).
+maxExp('Natureo', 125).
+maxExp('Volteo', 100).
+maxExp('Aereo',145).
+maxExp('Terreo', 150).
+maxExp('Camcam', 110).
+maxExp('Frogfrog', 115).
+maxExp('Busew', 120).
+maxExp('Lightkid', 135).
+maxExp('Skyling', 100).
+maxExp('Gibel', 110).
+maxExp('Ponytak', 115).
+maxExp('Trapezoidyu', 135).
+maxExp('Telorcute', 105).
+maxExp('Proton', 150).
+maxExp('Dodol', 110).
+maxExp('Oneng', 120).
